b544f253f23c4b8424f8a7436b94d861315dbc9f
Emanuele Bartolesi <bartolesiemanuele@hotmail.com>
2019-07-17 13:15:08 +0200

    first release

---
+++ src/ImmersiveReaderCore/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ImmersiveReaderCore.Models;
+using Microsoft.Extensions.Configuration;
+
+namespace ImmersiveReaderCore.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly string SubscriptionKey;
+        private readonly string Endpoint;
+
+        public HomeController(IConfiguration configuration)
+        {
+            SubscriptionKey = configuration["SubscriptionKey"];
+            Endpoint = configuration["Endpoint"];
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("token")]
+        public async Task<string> Token()
+        {
+            using (var client = new System.Net.Http.HttpClient())
+            {
+                client.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Key", SubscriptionKey);
+                using (var response = await client.PostAsync(Endpoint, null))
+                {
+                    var content = await response.Content.ReadAsStringAsync();
+
+                    return content;
+                }
+            }
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ src/ImmersiveReaderCore/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ImmersiveReaderCore.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
